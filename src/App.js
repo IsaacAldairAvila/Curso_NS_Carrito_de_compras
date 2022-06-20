af8273167ffd2bac313a6fb5c6e103f4842ba414
Isaac Avila <isaacaldairavila@gmail.com>
2022-06-20 15:58:16 -0500

    correccion de imagenes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Producto from './Components/Producto';
 import Layout from './Components/Layout';
 import Title from './Components/Title';
 import Navbar from './Components/Navbar'
+import tomate from './img/productos/tomate.jpg'
+import lechuga from './img/productos/lechuga.jpg'
+import arbejas from './img/productos/arbejas.jpg'
 class App extends Component {
   state = {
     productos: [
-      { name: 'Tomate', price: 150, img: '/productos/tomate.jpg' },
-      { name: 'Arbejas', price: 150, img: '/productos/arbejas.jpg' },
-      { name: 'Lechuga', price: 150, img: '/productos/lechuga.jpg' }
+      { name: 'Tomate', price: 150, img: tomate },
+      { name: 'Arbejas', price: 150, img: arbejas },
+      { name: 'Lechuga', price: 150, img: lechuga }
     ]
   }
   render() {
